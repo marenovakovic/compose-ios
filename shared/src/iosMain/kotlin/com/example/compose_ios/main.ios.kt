c02b962d40ceaf8aa10907b546ea82777704b0c9
Vova Stelmashchuk <smallstells@gmail.com>
2023-02-26 12:22:16 +0200

    Remove unused ios folder, and clean other project files

--- shared/src/iosMain/kotlin/com/example/compose_ios/main.ios.kt
+++ shared/src/iosMain/kotlin/com/example/compose_ios/main.ios.kt
@@ -3,7 +3,7 @@ package com.example.compose_ios
 import androidx.compose.ui.window.Application
 import platform.UIKit.UIViewController
 
-public fun MainViewController(): UIViewController =
+fun MainViewController(): UIViewController =
     Application("Example Application") {
         ExampleApplication()
     }
